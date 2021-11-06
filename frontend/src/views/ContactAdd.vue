1a84dbb356cf49f893352248e6ea6ed72a5b34d2
quockhangtran2001 <90443739+quockhangtran2001@users.noreply.github.com>
2021-11-06 15:06:02 +0700

    lab5 final
    lab5 final

---
+++ frontend/src/views/ContactAdd.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="edit-form">
+    <h4>Thêm Liên hệ</h4>
+    <ContactForm
+      :contact="contact"
+      @contact-submit="addContact"
+      @contact-delete="resetContact"
+    />
+    <p>{{ message }}</p>
+  </div>
+</template>
+<script>
+import ContactService from "../services/contact.service";
+import ContactForm from "../components/ContactForm";
+export default {
+  name: "ContactAdd",
+  components: {
+    ContactForm,
+  },
+  data() {
+    return {
+      contact: {
+        name: "Tran Quoc Khang",
+        email: "",
+        address: "",
+        phone: "",
+        favorite: false
+      },
+      message: "",
+    };
+  },
+  methods: {
+    initContact() {
+      // maybe not...      
+    },
+    async addContact(data) {
+      console.log("input: ", data);
+      const [error, response] = await this.handle(
+        ContactService.create(data)
+      );
+      if (error) {
+        console.log(error);
+      } else {
+        console.log(response.data);
+        this.message = "Liên hệ được thêm thành công.";
+      }
+    },
+    resetContact() {
+      alert("reset");
+    },
+  },
+  mounted() {
+    this.message = "";
+    this.initContact();
+  },
+};
+</script>
+<style>
+.edit-form {
+  max-width: 400px;
+  margin: auto;
+}
+</style>
